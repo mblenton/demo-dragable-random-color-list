f85a8b55bce70e7c1b59858525703dbbd332bb9e
Milan Rakos <mrakos@Milans-iMac-Pro.local>
2020-11-05 12:40:08 +0100

    main app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { useGetRandomColor } from './hooks/useGetRandomColor';
+import { useDragAndDrop } from './hooks/useDragAndDrop';
+import { EnterColor } from './components/EnterColor';
+import { Button } from './components/Button';
+import { DraggableList } from './components/DraggableList';
+import { pushToColorList } from './utils/pushToColorList';
 import './App.css';
 
+export type ColorsList = Array<string>;
+
 export const App = (): JSX.Element => {
+  const [colorsList, setColorsList] = useState<ColorsList>([]);
+  const [message, setMessage] = useState<string>('');
+  const [buttonText, setButtonText] = useState<string>('');
+  const [lastValidColor, setLastValidColor] = useState<string>('');
+  const { status, data, error, fetchData } = useGetRandomColor();
+  const draggableFuncs = useDragAndDrop({ colorsList, setColorsList });
+  const isFetching = status === 'fetching';
+
+  useEffect(() => {
+    if (status === 'fetched' && data?.new_color) {
+      // when entered input text and enter is not pressed -> change button text
+      setButtonText('Get new color');
+      setColorsList((colorsList) =>
+        pushToColorList({
+          colorsList,
+          newColor: `#${data.new_color}`,
+          setMessage,
+          setLastValidColor,
+        }),
+      );
+    }
+    if (error) {
+      setMessage(error);
+    }
+  }, [status, data, error]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <div className="messageContainer">{message}</div>
+      <div className="buttonContainer">
+        <Button
+          fetchData={fetchData}
+          isFetching={isFetching}
+          lastValidColor={lastValidColor}
+          buttonText={buttonText}
+        />
+      </div>
+      <div className="listContainer">
+        <DraggableList
+          colorsList={colorsList}
+          lastValidColor={lastValidColor}
+          draggableFuncs={draggableFuncs}
+        />
+      </div>
+      <div className="enterItemContainer">
+        <EnterColor
+          setColorsList={setColorsList}
+          setMessage={setMessage}
+          setButtonText={setButtonText}
+          setLastValidColor={setLastValidColor}
+        />
+      </div>
     </div>
   );
 };
